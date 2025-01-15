68599043edc61e6601a14ac7b9206384ef66180d
Mubashir Jilani <nk7674356@gmail.com>
2025-01-16 01:39:12 +0500

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+var cities = [
+    "Karachi", "Lahore", "Faisalabad", "Rawalpindi", "Multan",
+    "Hyderabad", "Gujranwala", "Peshawar", "Quetta",
+    "Islamabad", "Sialkot", "Bahawalpur", "Sukkur"
+];
+
+for (var i = 0; i < cities.length; i++) {
+    if (cities[i] === "Islamabad") {
+        alert("Islamabad is the cleanest city of Pakistan");
+    }
+}
